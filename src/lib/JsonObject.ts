b606604351cfa996e9c9ba3c9721d026115768e9
Sam Freeman <sam.freeman.55@gmail.com>
2023-03-12 12:05:42 -0400

    Changed JsonObject to interface

--- src/lib/JsonObject.ts
+++ src/lib/JsonObject.ts
@@ -1,6 +1,8 @@
 
 export type JsonValue = string | number | boolean | null
-export type JsonObject = { [key: string]: JsonValue | JsonValue[] | JsonObject | JsonObject[] }
+export interface JsonObject {
+	[key: string]: JsonValue | JsonObject | (JsonValue|JsonObject)[] 
+}
 
 export const isJsonObject = (value: unknown): value is JsonObject =>
 	!!value && typeof value == 'object' && !Array.isArray(value)
