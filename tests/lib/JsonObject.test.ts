b56c90625200b2bcf2404a1ce5b2eb161df09e03
Sam Freeman <sam.freeman.55@gmail.com>
2023-03-11 19:09:50 -0500

    Fixed spacing in JsonObject.test.ts

--- tests/lib/JsonObject.test.ts
+++ tests/lib/JsonObject.test.ts
@@ -3,12 +3,12 @@ import { isJsonObject } from '../../src/lib/JsonObject'
 
 describe('ts-jest-pnp', () => {
 	test('isJsonObject', () => {
-        expect(isJsonObject({})).toBe(true)
-        expect(isJsonObject([])).toBe(false)
-        expect(isJsonObject(null)).toBe(false)
-        expect(isJsonObject(undefined)).toBe(false)
-        expect(isJsonObject(0)).toBe(false)
-        expect(isJsonObject('')).toBe(false)
-        expect(isJsonObject(true)).toBe(false)
+		expect(isJsonObject({})).toBe(true)
+		expect(isJsonObject([])).toBe(false)
+		expect(isJsonObject(null)).toBe(false)
+		expect(isJsonObject(undefined)).toBe(false)
+		expect(isJsonObject(0)).toBe(false)
+		expect(isJsonObject('')).toBe(false)
+		expect(isJsonObject(true)).toBe(false)
 	})
 })
