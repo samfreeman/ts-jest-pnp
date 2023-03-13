5e2b9b5080df1284c4281a91c1b8f78715889343
Sam Freeman <sam.freeman.55@gmail.com>
2023-03-13 10:57:04 -0400

    Added TsProjectConfig

---
+++ src/TsProjectConfig.ts
@@ -0,0 +1,16 @@
+
+export interface TsProjectConfig {
+	parentFolder: string
+	name: string
+	outDir: string
+	srcDir: string
+	testsDir: string
+	testNameMatch: string
+	testsTsConfigName: string
+	version: string
+	description: string
+	author: string
+	email: string
+	keywords: string[]
+	license: string
+}
