ade0edbc0e016e2514ba9b4d9d56943b7d6aa1ac
Sam Freeman <sam.freeman.55@gmail.com>
2023-03-16 09:16:34 -0400

    Improved program flow

--- src/index.ts
+++ src/index.ts
@@ -7,14 +7,18 @@ import './lib/file'
 import { getTsProjectConfig } from './getTsProjectConfig'
 import { createTsProject } from './createTsProject'
 
+const rootFolder = __dirname.parentFolder()
+const pkgJson = JSON.parse(rootFolder.appendPath('package.json').readFile())
+const version = pkgJson.version ?? '1.0.0'
 
 '-- Create TypeScript Project --'.log()
-'-- Version 1.0.0'.log()
+'-- Version'.log(version)
+''.log()
+'Press Ctrl+C at any time to exit'.log()
 ''.log()
 
 const config = getTsProjectConfig()
-
-const prjFolder = config.name.resolvePath()
+const prjFolder = config.parentFolder.appendPath(config.name).resolvePath()
 
 ''.log()
 ''.log(`-- Creating project at '${prjFolder}'...`)
